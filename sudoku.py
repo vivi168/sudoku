ad19dc5c35230aac55ef7492cf0a6ec433354608
Vivien B <vivienbihl@gmail.com>
2017-12-29 23:25:50 +0100

    little optimization

--- sudoku.py
+++ sudoku.py
@@ -27,32 +27,6 @@ class Sudoku:
             line = file.read().replace(' ', '').replace('\n', '')
             return Sudoku(line)
 
-    def to_line(self):
-        s = ''
-        for i in self.grid:
-            s = s + str(i)
-        return s
-
-    def row(self, i):
-        c = []
-        for j in range(9):
-            c.append(self.grid[i * 9 + j])
-        return c
-
-    def column(self, i):
-        c = []
-        for j in range(9):
-            c.append(self.grid[j * 9 + i])
-        return c
-
-    def block(self, i):
-        c = []
-        b = 9 * (i - i % 3) + (i % 3 * 3)
-        for j in range(3):
-            for k in range(3):
-                c.append(self.grid[j * 9 + k + b])
-        return c
-
     def row_idx(self, i):
         return i // 9
 
@@ -62,18 +36,22 @@ class Sudoku:
     def block_idx(self, i):
         return self.row_idx(i) // 3 * 3 + self.column_idx(i) // 3
 
-    def is_valid(self):
-        if len(self.grid) != 81:
-            return False
-        if 0 in self.grid:
-            return False
+    def is_in_row(self, i, v):
+        for j in range(9):
+            if self.grid[i * 9 + j] == v: return True
+        return False
 
-        for i in range(9):
-            if len(set(self.row(i))) != 9 or len(set(
-                    self.column(i))) != 9 or len(set(self.block(i))) != 9:
-                return False
+    def is_in_col(self, i, v):
+        for j in range(9):
+            if self.grid[j * 9 + i] == v: return True
+        return False
 
-        return True
+    def is_in_blk(self, i, v):
+        b = 9 * (i - i % 3) + (i % 3 * 3)
+        for j in range(3):
+            for k in range(3):
+                if self.grid[j * 9 + k + b] == v: return True
+        return False
 
     def solve(self):
         if 0 not in self.grid:
@@ -82,9 +60,7 @@ class Sudoku:
         i = self.grid.index(0)
 
         for c in range(1, 10):
-            if c not in self.row(self.row_idx(i)) and c not in self.column(
-                    self.column_idx(i)) and c not in self.block(
-                        self.block_idx(i)):
+            if not self.is_in_row(self.row_idx(i), c) and not self.is_in_col(self.column_idx(i), c) and not self.is_in_blk(self.block_idx(i), c):
                 self.grid[i] = c
                 if self.solve():
                     return True
@@ -104,5 +80,3 @@ if __name__ == '__main__':
     print(s)
     s.solve()
     print(s)
-
-    print(s.is_valid())
